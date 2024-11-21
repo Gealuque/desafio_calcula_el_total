183cc3c92b30e6ad3a7131cc2a63cbbc46ae1134
Andreina Guerra <141190940+Gealuque@users.noreply.github.com>
2024-11-21 08:47:39 -0300

    comentarios

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,18 @@ precio = 400000
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
 
+/*Declaración de Variables*/
+
 const mas = document.querySelector('.boton_mas');
 const menos = document.querySelector('.boton_menos');
 const cantidad = document.querySelector('.cantidad');
 const total = document.querySelector('.valor-total')
 
+/*Iniciador de Contador*/
+
 let valorCantidad = 0;
 
+/*Acciones en los botones*/
 
 mas.addEventListener('click', () => {
     valorCantidad++;
